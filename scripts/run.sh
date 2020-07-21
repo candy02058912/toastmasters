a659cd0c4d71c8aae92cb2d4fdc996f52ee7097c
Candy Tsai <candy02058912@gmail.com>
2020-07-21 15:46:24 +0800

    add test script

---
+++ scripts/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function help() {
+  echo "Usage: [-v verbose]"
+  echo "-v: verbose"
+}
+
+# read flags.
+while getopts v:h option
+do
+  case $option in
+  v)  VERBOSE="true";;
+  *)  help
+      exit 1
+  esac
+done
+
+if [ "$VERBOSE" = "true" ]; then
+  ab -n 100 -c 100 -S -q 'localhost:32345/h1?a=1&b=3'
+else
+  ab -n 100 -c 100 -S -q 'localhost:32345/h1?a=1&b=3' | grep 'Time taken for tests:'
+fi
